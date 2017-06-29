c229a9e6528f719997a4bd566e6a893d75d9d3c3
aashish347 <aashish.kumar@elucidata.io>
2017-06-29 14:58:09 +0530

    import progress button module in  appmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MdProgressSpinnerModule } from '@angular/material';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MdIconModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
-import { ProgressButtonComponent } from './progress-button/progress-button.component'
+import { ProgressButtonModule } from './progress-button.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProgressButtonComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    MdProgressSpinnerModule,
-    MdIconModule
+    ProgressButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
