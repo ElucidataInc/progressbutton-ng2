ce1676404f68b81f55399472d29701a9d1190f4c
aashish347 <aashish.kumar@elucidata.io>
2017-06-29 11:49:50 +0530

    small demo for progress button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {State} from './state';
 
 @Component({
   selector: 'app-root',
@@ -6,14 +7,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  icon = "publish";
-  state = {
-      value: 'initial',
-      text: 'initial',
-      color: 'blue'
+
+  icon: string = "publish";
+
+  isDisabled: boolean = false;
+
+  stateNo: number = 0;
+
+  initialStateObject:  State = {
+  'value':'initial',
+  'text':'Upload',
+  'color':'#337ab7'
   };
-  isDisabled = false;
-  onClick():void {
-    console.log("click")
+
+  progressStateObject:  State = {
+  'value':'inprogress',
+  'text':'Uploading',
+  'color':'#a9a9a9'
+  };
+
+  doneStateObject:  State = {
+  'value':'done',
+  'text':'done',
+  'color':'#5cb85c'
+  };
+
+  errorStateObject:  State = {
+  'value':'error',
+  'text':'error',
+  'color':'#d9534f'
+  };
+
+  stateObject:  State[]  = [
+    this.initialStateObject,
+    this.doneStateObject,
+    this.errorStateObject,
+    this.progressStateObject
+  ];
+
+  toProgressState(): void{
+    this.stateNo = 3;
   }
+
+  toInitialState(): void{
+    this.stateNo = 0;
+  }
+
+  toDoneState(): void{
+    this.stateNo = 1;
+  }
+
+  toErrorState(): void{
+    this.stateNo = 2;
+  }
+
 }
