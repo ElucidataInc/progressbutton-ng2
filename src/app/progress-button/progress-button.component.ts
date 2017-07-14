497089638fe64c0b196bfe70b65152efe46aca04
aashish347 <aashish.kumar@elucidata.io>
2017-07-14 16:13:11 +0530

    use template and styles instead of templateUrl and styleUrls

--- src/app/progress-button/progress-button.component.ts
+++ src/app/progress-button/progress-button.component.ts
@@ -1,9 +1,85 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { State } from '../state';
+
+
 @Component({
   selector: 'progress-button',
-  templateUrl: './progress-button.component.html',
-  styleUrls: ['./progress-button.component.css']
+  template: `
+    <button class="button-dimension" [style.background-color]="color()" 
+        [disabled]="state.value == 'inprogress' || disabledstate==true" >
+        <md-spinner *ngIf="state.value == 'inprogress'" class="progress-circle" color="primary"></md-spinner>
+        <md-icon *ngIf="state.value == 'initial'" disabled="disabledstate==true" class="icon-fill">{{icon}}</md-icon>
+        <md-icon *ngIf="state.value == 'done'" disabled="disabledstate==true" class="icon-fill">check_circle</md-icon>
+        <md-icon *ngIf="state.value == 'error'" disabled="disabledstate==true" class="icon-fill">error</md-icon>
+        <span class="state-text" [style.vertical-align]="state.value == 'inprogress' ? 'middle' : 'super'" 
+            [attr.disabled]="state.value == 'inprogress' || disabledstate==true" >{{text()}}
+        </span>
+    </button>
+  `,
+  styles: [`
+    .state-text {
+        color: white;
+    }
+
+    .button-dimension {
+        color: #333;
+        background-color: #ccc;
+        border-color: #ccc;
+        min-height: 35px;
+        min-width: 112px; 
+        display: inline-block;
+        padding: 6px 12px;
+        margin-bottom: 0;
+        font-size: 14px;
+        font-weight: 400;
+        line-height: 1.42857143;
+        text-align: center;
+        white-space: nowrap;
+        vertical-align: middle;
+        cursor: pointer;
+        border: 1px solid transparent;
+        font-family: inherit;
+        text-transform: none;
+        overflow: visible;
+        margin: 0;
+        color: inherit;
+        align-items: flex-start;
+        text-rendering: auto;
+        letter-spacing: normal;
+        word-spacing: normal;
+        text-indent: 0px;
+        text-shadow: none;
+        font: inherit;
+        box-sizing: border-box;
+    }
+
+    .icon-fill {
+        color: #fff; 
+    }
+
+    .progress-circle {
+        width: 20px;
+        height: 20px;
+        margin: 0px;
+        vertical-align: middle;
+        display: inline-block;
+        pointer-events: none; 
+    }
+
+    md-icon[disabled] {
+        pointer-events: none; 
+    }
+
+    span[disabled] {
+        pointer-events: none; 
+    }
+
+    :disabled {
+        opacity: 0.65;
+        cursor: not-allowed;
+        box-shadow: none;
+    }`
+  ]
 })
 
 export class ProgressButtonComponent implements OnInit{
